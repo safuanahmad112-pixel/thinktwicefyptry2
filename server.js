b751aace8788aae6f0eaa6d2564db32cf94bae74
safuanahmad112-pixel <safuanahmad112@gmail.com>
2026-06-12 00:38:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -245,5 +245,5 @@ app.post("/reset-password", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log("✅ Server running on", PORT);
 });
